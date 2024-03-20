ec2bf1a0b38f9d046d5844685d77d049b2c54f9a
Carl Hari Alisangco <alisangcocarl@gmail.com>
2024-03-21 01:18:13 +0800

    milliseconds

--- src/app/api/ban/route.ts
+++ src/app/api/ban/route.ts
@@ -43,7 +43,7 @@ export async function POST(request: NextRequest) {
           userId: userId,
           reason: reason,
           email: email,
-          periodTime: startingTime,
+          periodTime: periodMilliseconds,
           permanent: perma,
         },
       });
